6b913973beca2963e6c75141d42921f8807036eb
Dariia Shamatrina <dshamatrina@gmail.com>
2020-04-28 12:24:19 +0300

    Updates

---
+++ precache-manifest.04ec5b37bd732b92b8ecbb85eeb5c0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3285b64c234ac68011f281477fbc0e",
+    "url": "/upgrade.test/index.html"
+  },
+  {
+    "revision": "4b799a868dbd22048056",
+    "url": "/upgrade.test/static/css/main.216f7419.chunk.css"
+  },
+  {
+    "revision": "5a44d7445497191a6d29",
+    "url": "/upgrade.test/static/js/2.0ed087c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/upgrade.test/static/js/2.0ed087c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b799a868dbd22048056",
+    "url": "/upgrade.test/static/js/main.b7546647.chunk.js"
+  },
+  {
+    "revision": "3695b559b88daeb7339e",
+    "url": "/upgrade.test/static/js/runtime-main.7ec40020.js"
+  }
+]);
