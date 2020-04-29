624149a11c50d736408f398bf8beba32f1e4a1c8
Dariia Shamatrina <dshamatrina@gmail.com>
2020-04-29 17:55:03 +0300

    Updates

---
+++ precache-manifest.0a0ad85b5631b2dc03890e7bf48b76c0.js
@@ -0,0 +1,33 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3285b64c234ac68011f281477fbc0e",
+    "url": "/upgrade.test/index.html"
+  },
+  {
+    "url": "/upgrade.test/precache-manifest.04ec5b37bd732b92b8ecbb85eeb5c0e1.js"
+  },
+  {
+    "revision": "b79efe6eaaf3b1ca3f63fba07646f176",
+    "url": "/upgrade.test/service-worker.js"
+  },
+  {
+    "revision": "4b799a868dbd22048056",
+    "url": "/upgrade.test/static/css/main.216f7419.chunk.css"
+  },
+  {
+    "revision": "5a44d7445497191a6d29",
+    "url": "/upgrade.test/static/js/2.0ed087c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/upgrade.test/static/js/2.0ed087c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b799a868dbd22048056",
+    "url": "/upgrade.test/static/js/main.b7546647.chunk.js"
+  },
+  {
+    "revision": "3695b559b88daeb7339e",
+    "url": "/upgrade.test/static/js/runtime-main.7ec40020.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("/upgrade.test/precache-manifest.0a0ad85b5631b2dc03890e7bf48b76c0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
